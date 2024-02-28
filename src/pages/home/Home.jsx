fbc150e99e4d04eada0ac49f8b3ecfac8fb18f01
FranciscoBisso <fjbiss@gmail.com>
2024-02-28 11:09:29 -0300

    Optimize videos, adjust slider's dimentions

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,10 @@ import poster from "../../assets/videos/videoframe.webp";
 
 import styles from "./home.module.css";
 // Videos
-import video_mobile from "../../assets/videos/bkg_480.webm";
-import video_tablet from "../../assets/videos/bkg_720.webm";
-import video_desktop from "../../assets/videos/bkg_1080.webm";
+import demo_480 from "../../assets/videos/short_demo_480.webm";
+import demo_720 from "../../assets/videos/short_demo_720.webm";
+import demo_1080 from "../../assets/videos/short_demo_1080.webm";
+import demo from "../../assets/videos/short_demo_original.webm";
 // Images
 import bgless_brand_negative from "../../assets/imgs/brand/logo/bgless_brand_negative.webp";
 
@@ -126,17 +127,22 @@ export default function Home() {
 					autoPlay
 					muted>
 					<source
-						src={video_mobile}
-						media="(max-width: 520px)"
+						src={demo_480}
+						media="(max-width: 500px)"
 						type="video/webm"
 					/>
 					<source
-						src={video_tablet}
+						src={demo_720}
+						media="(max-width: 1024px)"
+						type="video/webm"
+					/>
+					<source
+						src={demo_1080}
 						media="(max-width: 1368px)"
 						type="video/webm"
 					/>
 					<source
-						src={video_desktop}
+						src={demo}
 						type="video/webm"
 					/>
 				</video>
