35134d2dcd8874cdbb4928ce42f7da436108fc71
FranciscoBisso <fjbisso@gmail.com>
2024-03-03 12:36:04 -0300

    Add default font, adjust footer top border

--- src/components/footer/FooterContent.jsx
+++ src/components/footer/FooterContent.jsx
@@ -9,7 +9,7 @@ export default function FooterContent({ props }) {
 				San Martín, 750, Buenos Aires, CABA 1004
 			</span>
 			<span className={props?.styles?.txt}>
-				Copyright © 2024 LA Tech S.A.
+				Copyright © 2024 - LA Tech S.A.
 			</span>
 		</div>
 	);
