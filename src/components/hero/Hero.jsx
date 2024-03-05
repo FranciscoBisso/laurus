f02bcf99c2453af8280a991a20c32e69bfcfdf28
FranciscoBisso <fjbiss@gmail.com>
2024-03-05 20:20:54 -0300

    add responsibity to  hero_section & video_section user's color scheme preference

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import styles from "./hero.module.css";
+import bgless_brand_primary from "../../assets/imgs/brand/logo/bgless_brand_primary.webp";
 import bgless_brand_secondary from "../../assets/imgs/brand/logo/bgless_brand_secondary.webp";
 import reel_720 from "../../assets/videos/reels/farm_reel_720.webm";
 import reel_1080 from "../../assets/videos/reels/farm_reel_1080.webm";
 import poster from "../../assets/videos/reels/reel_frist_frame.webp";
 export default function Hero() {
+	const [isDarkMode, setIsDarkMode] = useState(
+		window.matchMedia("(prefers-color-scheme: dark)").matches
+	);
+
+	useEffect(() => {
+		const darkModeMediaQuery = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		);
+
+		const handleChange = (e) => {
+			setIsDarkMode(e.matches);
+		};
+
+		darkModeMediaQuery.addEventListener("change", handleChange);
+
+		return () => {
+			darkModeMediaQuery.removeEventListener("change", handleChange);
+		};
+	}, []);
+
 	return (
 		<section className={styles.hero_section}>
 			<div className={styles.logo_wrapper}>
-				<img
-					className={styles.logo}
-					src={bgless_brand_secondary}
-					alt="logo"
-				/>
+				{isDarkMode && (
+					<img
+						className={styles.logo}
+						src={bgless_brand_primary}
+						alt="logo"
+					/>
+				)}
+				{!isDarkMode && (
+					<img
+						className={styles.logo}
+						src={bgless_brand_secondary}
+						alt="logo"
+					/>
+				)}
 			</div>
 			<div className={styles.bg_wrapper}>
 				<video
