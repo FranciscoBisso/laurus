7eb317cbf61f9c08e585e3733fdd34c2c2f75da4
FranciscoBisso <fjbisso@gmail.com>
2024-03-04 23:07:13 -0300

    css: video_section

--- src/components/multimedia/Multimedia.jsx
+++ src/components/multimedia/Multimedia.jsx
@@ -1,12 +1,13 @@
 import styles from "./multimedia.module.css";
-import poster from "../../assets/videos/demos/videoframe.webp";
-import demo_480 from "../../assets/videos/demos/short_demo_480.webm";
-import demo_720 from "../../assets/videos/demos/short_demo_720.webm";
-import demo_1080 from "../../assets/videos/demos/short_demo_1080.webm";
-import demo from "../../assets/videos/demos/short_demo_original.webm";
+import reel_720 from "../../assets/videos/reels/tower_reel_720.webm";
+import reel_1080 from "../../assets/videos/reels/tower_reel_1080.webm";
+import poster from "../../assets/videos/reels/tower_reel_frist_frame.webp";
 export default function Multimedia() {
 	return (
 		<section className={styles.video_section}>
+			<p className={styles.txt}>
+				480,000 plants per year, in less space than a tennis court
+			</p>
 			<video
 				loading="lazy"
 				className={styles.video}
@@ -16,22 +17,12 @@ export default function Multimedia() {
 				autoPlay
 				muted>
 				<source
-					src={demo_480}
-					media="(max-width: 500px)"
-					type="video/webm"
-				/>
-				<source
-					src={demo_720}
+					src={reel_720}
 					media="(max-width: 1024px)"
 					type="video/webm"
 				/>
 				<source
-					src={demo_1080}
-					media="(max-width: 1368px)"
-					type="video/webm"
-				/>
-				<source
-					src={demo}
+					src={reel_1080}
 					type="video/webm"
 				/>
 			</video>
