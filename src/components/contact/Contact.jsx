6b955cca20b1919abd05d85b2ba92903d6a91bcd
FranciscoBisso <fjbisso@gmail.com>
2024-03-07 15:26:02 -0300

    styling Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,16 +5,15 @@ export default function Contact() {
 	return (
 		<section className={styles.contact_section}>
 			<article className={styles.contact_card}>
-				<div className={styles.contact_card_body}>
-					<h1 className={styles.card_header}>
-						{"Together, let's pioneer the future of agriculture"}
-					</h1>
-					<p className={styles.card_text}>
-						Join us today and create a future where fresh food is
-						accessible to all, regardless of where they live or how
-						much they make.
-					</p>
-				</div>
+				<h1 className={styles.card_header}>
+					{"Let's pioneer the future of agriculture, together"}
+				</h1>
+				<p className={styles.card_text}>
+					Join us today and create a future where fresh food is
+					accessible to all, regardless of where they live or how much
+					they make.
+				</p>
+
 				<PopupButton
 					className={styles.calendly_btn}
 					url="https://calendly.com/nicobisso/intro-call"
