8b260a967760a14d30ca3ced2847a16d0e3d6a2d
FranciscoBisso <fjbiss@gmail.com>
2024-03-27 19:15:50 -0300

    update ContactSkeleton

--- src/components/contactSection/ContactSkeleton.jsx
+++ src/components/contactSection/ContactSkeleton.jsx
@@ -7,15 +7,6 @@ export default function ContactSkeleton() {
 			tabIndex={0}>
 			<div className={styles.card_wrapper}>
 				<article className={styles.contact_card}>
-					<h1 className={styles.card_header}>
-						{"Let's pioneer the future of agriculture, together"}
-					</h1>
-					<p className={styles.card_text}>
-						Join us today and create a future where fresh food is
-						accessible to all, regardless of where they live or how
-						much they make.
-					</p>
-
 					<div className={styles.btn_skeleton}>
 						<p className={styles.btn_skeleton_txt}>
 							Schedule a call!
