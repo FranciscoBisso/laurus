0cf9c7b566a2f8edab6820d79d6a1d0392d9b843
FranciscoBisso <fjbisso@gmail.com>
2024-04-08 19:21:03 -0300

    Updates

---
+++ assets/Spinner-49m7nW0r.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-EMM1wVOD.js";const a="_loader_wrapper_cg5d4_1",o="_loader_cg5d4_1",p="_rotate_cg5d4_1",t="_prixClipFix_cg5d4_1",e={loader_wrapper:a,loader:o,rotate:p,prixClipFix:t};function l(){return r.jsx("div",{className:e.loader_wrapper,children:r.jsx("span",{className:e.loader})})}export{l as default};
