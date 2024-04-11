a489594610775fe307cf40cbff466ec91ea27e31
FranciscoBisso <fjbisso@gmail.com>
2024-04-11 10:11:27 -0300

    Quit lazy loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,68 +1,20 @@
-import { lazy, Suspense } from "react";
 import styles from "./home.module.css";
-
-import Loader from "../../components/loader/Loader";
-const IntroSkeleton = lazy(() =>
-	import("../../components/introSection/skeleton/IntroSkeleton")
-);
-const PurposeSkeleton = lazy(() =>
-	import("../../components/purposeSection/skeleton/PurposeSkeleton")
-);
-const VideoSkeleton = lazy(() =>
-	import("../../components/videoSection/skeleton/VideoSkeleton")
-);
-const ConclusionSkeleton = lazy(() =>
-	import("../../components/conclusionSection/skeleton/ConclusionSkeleton")
-);
-const ContactSkeleton = lazy(() =>
-	import("../../components/contactSection/skeleton/ContactSkeleton")
-);
-const Spinner = lazy(() => import("../../components/spinner/Spinner"));
-
-const IntroSection = lazy(() =>
-	import("../../components/introSection/IntroSection")
-);
-const PurposeSection = lazy(() =>
-	import("../../components/purposeSection/PurposeSection")
-);
-const VideoSection = lazy(() =>
-	import("../../components/videoSection/VideoSection")
-);
-const ConclusionSection = lazy(() =>
-	import("../../components/conclusionSection/ConclusionSection")
-);
-const ContactSection = lazy(() =>
-	import("../../components/contactSection/ContactSection")
-);
-const Footer = lazy(() => import("../../components/footer/Footer"));
+import IntroSection from "../../components/introSection/IntroSection";
+import PurposeSection from "../../components/purposeSection/PurposeSection";
+import VideoSection from "../../components/videoSection/VideoSection";
+import ConclusionSection from "../../components/conclusionSection/ConclusionSection";
+import ContactSection from "../../components/contactSection/ContactSection";
+import Footer from "../../components/footer/Footer";
 
 export default function Home() {
 	return (
 		<div className={styles.home_wrapper}>
-			<Suspense fallback={<Loader />}>
-				<Suspense fallback={<IntroSkeleton />}>
-					<IntroSection />
-				</Suspense>
-
-				<Suspense fallback={<PurposeSkeleton />}>
-					<PurposeSection />
-				</Suspense>
-
-				<Suspense fallback={<VideoSkeleton />}>
-					<VideoSection />
-				</Suspense>
-
-				<Suspense fallback={<ConclusionSkeleton />}>
-					<ConclusionSection />
-				</Suspense>
-
-				<Suspense fallback={<ContactSkeleton />}>
-					<ContactSection />
-				</Suspense>
-				<Suspense fallback={<Spinner />}>
-					<Footer />
-				</Suspense>
-			</Suspense>
+			<IntroSection />
+			<PurposeSection />
+			<VideoSection />
+			<ConclusionSection />
+			<ContactSection />
+			<Footer />
 		</div>
 	);
 }
